bbc10dc59e4c01b8c854543d68868df5d0d7eab8
Misha <97121455+Mike1ssoN@users.noreply.github.com>
2022-09-13 20:49:24 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+   public IActionResult Index()
+   {
+      return View();
+   }
+   [HttpGet]
+   public ViewResult RsvpForm()
+   {
+      return View();
+   }
+
+   [HttpPost]
+   public ViewResult RsvpForm(GuestResponse questResponse)
+   {
+     Repository.AddResponses(questResponse);
+     return View("Thanks", questResponse);
+   }
+
+   public IActionResult ListRespinses()
+   {
+      return View(Repository.Responses.Where(x => x.WillAttend == true));
+   }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,14 @@
+﻿namespace PartyInvites.Models;
+
+using System.Collections.Generic;
+
+public static class Repository
+{
+    private static List<GuestResponse> _responses = new List<GuestResponse>();
+    public static IEnumerable<GuestResponse> Responses => _responses;
+
+    public static void AddResponses(GuestResponse response)
+    {
+        _responses.Add(response);
+    }
+}
